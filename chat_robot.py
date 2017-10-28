8ab69fe9cefc81169b1940c636673f839a0ddc7f
chenlianqing <lianqing_chan@126.com>
2017-10-29 00:06:46 +0800

    文件拆分成收件人部分-函数部分-数据部分

--- chat_robot.py
+++ chat_robot.py
@@ -9,25 +9,17 @@ import itchat
 itchat.auto_login()
 
 
-my_nicknames = [u'能小逗',u'陈晚清']
-my_userNamelist = [u'filehelper']
-
-def get_realid(nicknames,userNamelist):
+# 从昵称获取真实用户ID并发送信息
+def send_wechat(nicknames,userNamelist):
     for i in nicknames:
         users = itchat.search_friends(name=i)
         userNamelist.append(users[0]['UserName'])
-        nicknames.insert(0,u'陈廉清')
+    nicknames.insert(0,u'陈廉清')
+    for j in userNamelist:
+        itchat.send(u'%s，您好，我是陈廉清的微信机器人。'%nicknames[userNamelist.index(j)],toUserName = j)
 
-get_realid(my_nicknames,my_userNamelist)
 
-
-my_nicknames.insert(0,u'陈廉清')
-
-print(my_nicknames)
-print(my_userNamelist)
-
-for j in userNamelist:
-    itchat.send(u'%s，您好，我是陈廉清的微信机器人。'%nicknames[userNamelist.index(j)],toUserName = j)
+send_wechat(my_nicknames,my_userNamelist)
 
 
 
---
+++ my_friends.py
@@ -0,0 +1,6 @@
+#! /Users/chenlianqing/anaconda/bin python3
+# -*- coding: utf-8 -*-
+
+
+my_nicknames = [u'能小逗',u'陈晚清']
+my_userNamelist = [u'filehelper']
